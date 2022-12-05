f89c3ebf10b7fa3957b1a90501165cbd6034c599
nirShv <nir1shv@gmail.com>
2022-12-05 11:33:33 +0200

    password

--- config/prod.js
+++ config/prod.js
@@ -1,6 +1,6 @@
 module.exports = {
   // 'dbURL': 'mongodb+srv:',
-  // 'dbURL': `mongodb+srv://nir1shv:<1qaz@WSX>@strello.5rjc0u6.mongodb.net/?retryWrites=true&w=majority`
-  'dbURL': `mongodb+srv://nir1shv:1qaz@WSX@strello.5rjc0u6.mongodb.net/?retryWrites=true&w=majority`
+  'dbURL': `mongodb+srv://nir1shv:052795816@strello.5rjc0u6.mongodb.net/?retryWrites=true&w=majority`
+  // 'dbURL': `mongodb+srv://nir1shv:1qaz@WSX@strello.5rjc0u6.mongodb.net/?retryWrites=true&w=majority`
 }
  
